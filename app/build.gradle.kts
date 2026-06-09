9352411a6b847f70ffe9f7c7a135eadf321021a4
ferlagod <info@bibliohouse.org>
2026-06-09 20:57:26 +0200

    feat: add project baseline profiles, configure build dependencies, update code styles, and initialize theme assets.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,6 +9,10 @@ android {
         version = release(36) {
             minorApiLevel = 1
         }
+        dependenciesInfo {
+            includeInApk = false
+            includeInBundle = false
+        }
     }
 
     defaultConfig {
