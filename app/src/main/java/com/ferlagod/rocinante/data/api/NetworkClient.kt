29618e9605acb501d41441388423e7348f28c957
ferlagod <info@bibliohouse.org>
2026-06-11 21:41:52 +0200

    feat: se corrige error 403 al buscar un libro manualmente por isbn, se utiliza un agent user para ello. Además se corrige problema de iconos blancos en roms modificadas

--- app/src/main/java/com/ferlagod/rocinante/data/api/NetworkClient.kt
+++ app/src/main/java/com/ferlagod/rocinante/data/api/NetworkClient.kt
@@ -408,6 +408,7 @@ object NetworkClient {
 
             val requestBuilder = chain.request().newBuilder()
                 .addHeader("Referer", finalUrl)
+                .addHeader("User-Agent", "Mozilla/5.0 (Linux; Android 10; Mobile) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36 Rocinante/1.0")
 
             val acceptHeader = chain.request().header("Accept")
             if (acceptHeader?.contains("text/html") != true) {
