63b7a2d62b151bfddcc44c6721596d07134f1cf2
ferlagod <info@bibliohouse.org>
2026-07-11 15:50:34 +0200

    chore: nueva versión 1.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.ferlagod.rocinante"
         minSdk = 26
         targetSdk = 36
-        versionCode = 17
-        versionName = "1.1.1"
+        versionCode = 18
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
