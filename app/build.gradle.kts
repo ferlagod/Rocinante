374123451fae9287284e59ac2f58d4341bbc225f
ferlagod <proyectos@oniros.eu>
2026-07-22 15:33:38 +0200

    chore: nueva versión 1.1.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.ferlagod.rocinante"
         minSdk = 26
         targetSdk = 37
-        versionCode = 23
-        versionName = "1.1.6"
+        versionCode = 24
+        versionName = "1.1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
