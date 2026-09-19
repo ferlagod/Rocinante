c1adaeb9ee4ca63d789c890ffdb85c82839db865
ferlagod <proyectos@oniros.eu>
2026-09-19 17:48:56 +0200

    chore: nueva versión 1.3.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.ferlagod.rocinante"
         minSdk = 26
         targetSdk = 37
-        versionCode = 44
-        versionName = "1.3.2"
+        versionCode = 45
+        versionName = "1.3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
