ec1b19ce1bd2f0c1e14b234116104e4726ca30b7
ferlagod <proyectos@oniros.eu>
2026-09-26 17:38:35 +0200

    chore: nueva version 1.3.4

--- app/src/main/java/com/ferlagod/rocinante/ui/components/ChangelogDialog.kt
+++ app/src/main/java/com/ferlagod/rocinante/ui/components/ChangelogDialog.kt
@@ -40,10 +40,10 @@ import com.ferlagod.rocinante.R
  */
 object Changelog {
     /** Versión a la que corresponde el texto de abajo. */
-    const val CURRENT_VERSION = "1.3.1"
+    const val CURRENT_VERSION = "1.3.4"
 
     /** Texto de las novedades de [CURRENT_VERSION]. */
-    val textRes = R.string.changelog_text_v1_3_1
+    val textRes = R.string.changelog_text_v1_3_4
 }
 
 /**
